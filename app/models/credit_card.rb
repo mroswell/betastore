fc8656fdedb75892de13f8dadcd76c8237c90ecc
Margie Roswell <mroswell@gmail.com>
2014-03-24 00:13:16 -0400

    re-enable numericality validation for card_number

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -5,7 +5,7 @@ class CreditCard < ActiveRecord::Base
 
   validates :card_number, presence: true
   validates :card_number, length: { minimum: 15 }
- # validates :card_number, numericality: true
+  validates :card_number, numericality: true
   validates :expiration_date, format: { with: /\A\d{2}\/\d{4}\z/, message: "Only MM/YYYY allowed" }
   #validates :expires_on, numericality { greater_than: Time.now }
 #  validates :expires_on, :numericality => {:greater_than => Time.now}
